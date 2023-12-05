50e63cf3913b6d5279863b1ae6bc4acb2a2b4906
thasneem1234 <thasneem648@gmail.com>
2023-12-05 20:09:58 +0530

    hh

---
+++ todoproject/todoapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Task(models.Model):
+    name=models.CharField(max_length=250)
+    priority=models.IntegerField()
+    date=models.DateField()
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+# Create your models here.
---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+
+
+    path('',views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+
+
+]
